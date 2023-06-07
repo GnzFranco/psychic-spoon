496932ce7f0bb9cc937ee517a55672299b5bba2e
Franco Gonzalez <gnzfranco@hotmail.com>
2023-06-06 23:28:38 -0300

    empezando clase 2
    falta continuar desde el minuto 24

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,49 @@ const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6595ed,
-	width: 650,
-	height: 480
+	backgroundColor: "#32a84a",
+	width: 1920,
+	height: 1080
 });
+
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	const canvas = app.view as HTMLCanvasElement;
+
+	canvas.style.width = gameWidth + "px";
+	canvas.style.height = gameHeight + "px";
+
+	canvas.style.marginLeft = marginHorizontal + "px";
+	canvas.style.marginRight = marginHorizontal + "px";
+
+	canvas.style.marginTop = marginVertical + "px";
+	canvas.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+
 
 Assets.add("Clampy", "./clampy.png");
 Assets.add("Mega", "./mega1.png");
+Assets.add("Dino", "./dino1.png");
 
-Assets.load(["Clampy", "Mega"]).then(()=>{
+Assets.load(["Clampy", "Mega", "Dino"]).then(()=>{
 
-	const pj: Sprite = Sprite.from("Mega");
+	const pj: Sprite = Sprite.from("Dino");
 	//const pj2: Sprite = Sprite.from("Clampy");
 
 	pj.anchor.set(0.5);
 
-	pj.x = app.screen.width /2;
+	pj.x = app.screen.width /3;
 	pj.y = app.screen.height /2;
 
 	console.log ("Hola mundo!", pj.width, pj.height);
